e93e4f0a7d6b44412ad11a63a9ac064d3939bb5a
arash bahariye <bahariye.arash@gmail.com>
2023-10-14 18:41:27 +0200

    add vehicle resource routes

--- routes/api.php
+++ routes/api.php
@@ -27,4 +27,5 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('profile', [Auth\ProfileController::class, 'show']);
     Route::put('profile', [Auth\ProfileController::class, 'update']);
     Route::put('password', Auth\PasswordUpdateController::class);
+    Route::apiResource('vehicles', VehicleController::class);
 });
