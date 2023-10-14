997cbfccd284bab7c9c4a430be5ad2fa4adeeab4
arash bahariye <bahariye.arash@gmail.com>
2023-10-14 23:44:32 +0200

    add parkings routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\V1\Auth;
+use App\Http\Controllers\Api\V1\ParkingController;
+use \App\Http\Controllers\Api\V1\VehicleController;
+use App\Http\Controllers\Api\V1\ZoneController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,10 +25,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/logout', Auth\LogoutController::class);
 Route::post('auth/login', Auth\LoginController::class);
+Route::get('zones', [ZoneController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::get('profile', [Auth\ProfileController::class, 'show']);
     Route::put('profile', [Auth\ProfileController::class, 'update']);
     Route::put('password', Auth\PasswordUpdateController::class);
     Route::apiResource('vehicles', VehicleController::class);
+    Route::post('parkings/start', [ParkingController::class, 'start']);
+    Route::get('parkings/{parking}', [ParkingController::class, 'show']);
+    Route::put('parkings/{parking}', [ParkingController::class, 'stop']);
 });
